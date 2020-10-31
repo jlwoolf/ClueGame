48a1f2d96ac66b23ac4a6e1d3a910a2b37c38104
Jonathan Woolf <jlwoolf@mymail.mines.edu>
2020-10-30 20:55:44 -0600

    Changes to be committed: Modified the exception class to allow for printing a message so I can specify the logistics of an error. 	modified:   src/clueGame/BadConfigFormatException.java

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -5,10 +5,7 @@ public class BadConfigFormatException extends Exception{
 	public BadConfigFormatException() {
 		super("Bad Configuaration Formatting");
 	}
-	public BadConfigFormatException(char roomChar) {
-		super("Configuration file contains invalid room");
-	}
-	public BadConfigFormatException(int col1, int col2) {
-		super("Column values do not match for each row");
+	public BadConfigFormatException(String message) {
+		super(message);
 	}
 }
